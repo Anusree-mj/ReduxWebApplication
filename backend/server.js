c42791a3e4fab7bb3458b74f67962333bb2acb25
Anusree-mj <anuanusree682@gmail.com>
2024-03-21 00:43:07 +0530

    fix bugs in update profile

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDb from './config/db.js';
 dotenv.config();
@@ -13,6 +14,7 @@ import adminRoutes from './routes/adminRoutes.js';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes)
 app.use('/admin', adminRoutes);
