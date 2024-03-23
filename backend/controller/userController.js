cda53bda79e3f0e460905e0f540402742d23a960
Anusree-mj <anuanusree682@gmail.com>
2024-03-23 10:46:38 +0530

    delete frontend

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -79,7 +79,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 // update user
 const updateUserProfile = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.body._id);
+    const user = await User.findById(req.user._id);
 
     if (user) {
         user.name = req.body.name || user.name;
